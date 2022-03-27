f8dd40a53fab0f33fd633086b4dd63019c84f14a
Saad Khan <100066385+Saadkhanzada@users.noreply.github.com>
2022-03-27 19:57:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,122 @@
+// var num1 = +prompt("enter num1")
+// var opt = prompt("enter opt")
+// var num2 = +prompt("enter num2")
+
+// if(opt === "+"){
+//     var add = num1 + num2
+//     console.log(add);
+// }
+// else if(opt === "-"){
+//     var add = num1 - num2
+//     console.log(add);
+// }
+// else if(opt === "*"){
+//     var add = num1 * num2
+//     console.log(add);
+// }
+
+
+// ---------assigment 1------------
+
+// alert ()
+
+// alert ("HELLO SIR")
+
+// alert ("HELLO \n CLASS")
+
+// alert("HELLO CLASS")
+
+// alert("HELLO WORLD")
+
+// console.log("HELLO CLASS")
+
+
+
+
+// ---------------assigment 2--------------
+
+// var  "username"
+
+// var firstname = "SAAD"
+// var lastname = "KHAN"
+// var Fullname = firstname + " " + lastname
+
+// var message = "HELLO WORLD"
+// alert(message)
+
+// var name1 = "SAAD KHAN"
+// var age = "18 year old"
+// var text = "WEB DEVELOPER"
+// alert(name1)
+// alert(age)
+// alert(text)
+
+// var name = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(name)
+
+// var email = "SaadKhan@gmail.com"
+// alert(email)
+
+// var book = "A SMARTER WAY TO LEARN JS"
+// alert(book)
+
+// document.write("<h1>JAVASCRIPT</h1>")
+
+// document.write("<h1>JAVASCRIPT</h1>")
+// alert("JAVASCRIPT")
+
+
+
+// -------------assigment 3---------------------
+
+// var age = "i am 18 year old"
+// alert(age)
+
+// var birthyear = ("My birth year is 2003") 
+// document.write(birthyear)
+
+
+// var visitorname = ("Saad khan")
+// var prodtitle = ("ordered 5-Tshirts on")
+// var quant = ("xyz-clothing")
+// document.write(visitorname + " " + prodtitle + " " + quant)
+
+// -----------------assigment 4------------------
+
+// legal name = name $name myname my_name 
+// illegal name = 1name ?name my name 
+
+
+// document.write("<h1>Rules for naming js variables</h1>")
+// document.write("<p> Variable names can only contain number, $, and_ .For example $my_1stVariable</p>")
+// document.write("<p>Variables must begin with a letter, $ or _. For example $name, _name or name</p>")
+// document.write("<p>Variable names are case sensitive</p>")
+// document.write("<p>Variable names should not be JS keyword</p>")
+
+
+// var myname $name my_name
+
+// --------------------assigment 5----------------
+
+// document.write("<h1>The Lifetime Supply Calculator:</h1>")
+// document.write("<p>Favourite Snack : chocalte chip</p>")
+// document.write("<p>Current age: 18</p>")
+// document.write("<p>Amount of snack per day: 5</p>")
+
+
+
+// ----------------assigment 8 --------------
+
+// var greeting;
+// var hour = 13;
+// if (hour <= 18){
+
+//     greeting = "Good day";
+// }
+// else{
+// greeting = "Good evening";
+// }
+
+
+
+
